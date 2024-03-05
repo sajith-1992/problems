52d86da561be8eb24f068ea1579209b1fa06ce67
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-05 12:04:14 -0500

    minmax sum

--- minmaxsum.py
+++ minmaxsum.py
@@ -1,12 +1,9 @@
 # to find min and max sum 
 def miniMaxSum(arr):
     arr.sort()
-    minimumsum = sum(arr[:4])
-    maximumsum = sum(arr[1:])
-    print(minimumsum)
-    print(maximumsum)
-def main():
-  arr=[1,2,3,4,5]
-  miniMaxSum(arr)
+    minimum_sum = sum(arr[:4])
+    maximum_sum = sum(arr[1:]) 
+    print(minimum_sum,maximum_sum)
+      
   
-main()
+miniMaxSum()
