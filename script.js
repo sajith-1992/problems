cea45d9adbcca312031ffcffe747bfeb89523196
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-07 16:35:09 -0500

    javascript calculator

---
+++ script.js
@@ -0,0 +1,9 @@
+function show(val){
+     document.getElementById("screen_value").value+= val
+}
+function minus(){
+    minus_oldval = document.getElementById("screen_value").value
+    document.getElementById("screen_value").value=""
+  
+    
+}
