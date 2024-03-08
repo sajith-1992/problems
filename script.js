25aac94d0cccce6a5104b6e83666b0d34ac3519f
Sajith Mohan Plavalappil <sajithmohan1991992@gmail.com>
2024-03-08 10:12:39 -0500

    jquery

--- script.js
+++ script.js
@@ -2,8 +2,21 @@ function show(val){
      document.getElementById("screen_value").value+= val
 }
 function minus(){
-    minus_oldval = document.getElementById("screen_value").value
+  var  minus_oldval = document.getElementById("screen_value").value
     document.getElementById("screen_value").value=""
+    
+    eql(minus_oldval,calculator)
+    
+
   
     
+}
+function eql(){
+     var new_value=document.getElementById("screen_value").value
+     console.log(new_value)
+    switch (calculator){
+        case 1:
+           var result= new_value-minus_oldval
+    }
+    document.getElementById("screen_value").value=result
 }
